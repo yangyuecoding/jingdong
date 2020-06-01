553eb7cc8b2dc65d77b1731abac72b64f6fbc605
yangyuecoding <951921220@qq.com>
2020-06-01 14:50:33 +0800

    第二次更新版本

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+/*
+ * @Description: 越哥哥的小代码,看看就行了
+ * @Version: 版本更新中...
+ * @Autor: YangYi
+ * @Date: 2020-06-01 13:41:10
+ * @LastEditors: YangYi
+ * @LastEditTime: 2020-06-01 14:48:12
+ */ 
+
+let gulp = require("gulp");
+let htmlmin = require("gulp-htmlmin");
+let cssmin = require("gulp-clean-css");
+let jsmin = require("gulp-uglify");
+let babel = require("gulp-babel");
+
+
+gulp.task("default",async()=>{    
+  
+
+    gulp.watch("./newProject/html/*.html",async ()=>{
+        gulp.src("./newProject/html/*.html")
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeEmptyAttributes: true,
+            minifyJS: true,
+            minifyCSS:true
+        }))
+        .pipe(gulp.dest("D:\\phpstudy_pro\\WWW\\jingdong\\html"))
+    })
+
+    gulp.watch("./newProject/index.html",async ()=>{
+        gulp.src("./newProject/index.html")
+        .pipe(htmlmin({
+            collapseWhitespace: true,
+            removeEmptyAttributes: true,
+            minifyJS: true,
+            minifyCSS:true
+        }))
+        .pipe(gulp.dest("D:\\phpstudy_pro\\WWW\\jingdong"))
+    })
+
+    gulp.watch("./newProject/api/**/*",async ()=>{
+        gulp.src("./newProject/api/**/*")
+        .pipe(gulp.dest("D:\\phpstudy_pro\\WWW\\jingdong\\api"))
+    })
+    gulp.watch("./newProject/css/*.css",async ()=>{
+        gulp.src("./newProject/css/*.css")
+        .pipe(cssmin())
+        .pipe(gulp.dest("D:\\phpstudy_pro\\WWW\\jingdong\\css"))
+    })
+
+    gulp.watch("./newProject/js/*.js",async ()=>{
+        gulp.src("./newProject/js/*.js")
+        .pipe(gulp.dest("D:\\phpstudy_pro\\WWW\\jingdong\\js"))
+    })
+ 
+    gulp.src("./newProject/imgs/**/*")
+    .pipe(gulp.dest("D:\\phpstudy_pro\\WWW\\jingdong\\imgs"))
+    gulp.src("./newProject/libs/**/*")
+    .pipe(gulp.dest("D:\\phpstudy_pro\\WWW\\jingdong\\libs"))
+    
+})
